74c861075282ab2d7b751a991a6bd838808916ca
hiren patel <hirenpatel0526@yahoo.com>
2022-10-08 14:28:31 -0700

    manager test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,4 +1,4 @@
-const engineer = require(./lib/engineer.js);
+const engineer = require(./lib/Engineer.js);
 test ("is for engineer section", ()=>{
     const engineer = new Engineer("rahul", 4444, "rahul@mail.com", "rahuldesai");
     expect(engineer.github).toEqual(expect.any(string));
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,4 +1,4 @@
-const intern = require("./lib/intern.js");
+const intern = require("./lib/Intern.js");
 test("is for intern section", ()=>{
     const intern = new Intern("rushi", 3333, "Upper Iowa University");
     expect (intern.school).toEqual(expect.any(string));
