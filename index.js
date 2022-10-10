8c32227ab2873c00debcb40b7ee86f6d819f3dc3
hiren patel <hirenpatel0526@yahoo.com>
2022-10-10 13:30:46 -0700

    added write file

--- index.js
+++ index.js
@@ -128,6 +128,18 @@ const newEmployee = () => {
     }
     )
 };
+
+const writeFile =fileContent => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+        if (err){
+            console.log(err);
+            return;
+        } else {
+            console.log ('File created');
+        }
+    });
+};
+
         
 
 
