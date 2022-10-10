4cf89bdc164052905d188aa4a410456627e6a733
hiren patel <hirenpatel0526@yahoo.com>
2022-10-10 10:24:47 -0700

    require

---
+++ index.js
@@ -0,0 +1,6 @@
+ const fs = require("fs");
+ const enquirer = require("inquirer");
+ const employee = require("./lib/Employee");
+ const manager = require("./lib/Manager");
+ const engineer = require("./lib/Engineer");
+ const intern = inquire("./lib/intern");
